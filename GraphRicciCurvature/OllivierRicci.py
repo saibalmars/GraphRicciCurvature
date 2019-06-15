e294190c4beaeb83a1b47cfd232602b29a785a89
saibalmars <saibalmars@gmail.com>
2019-06-14 17:24:36 -0700

    Add license

--- GraphRicciCurvature/OllivierRicci.py
+++ GraphRicciCurvature/OllivierRicci.py
@@ -2,9 +2,21 @@
 A NetworkX addon program to compute the Ollivier-Ricci curvature of a given NetworkX graph.
 
 
+Copyright 2018 Chien-Chun Ni
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 
 Author:
-
     Chien-Chun Ni
     http://www3.cs.stonybrook.edu/~chni/
 
--- GraphRicciCurvature/RicciFlow.py
+++ GraphRicciCurvature/RicciFlow.py
@@ -1,3 +1,30 @@
+"""
+A NetworkX addon program to compute the Ollivier-Ricci Flow of a given NetworkX graph.
+
+
+Copyright 2018 Chien-Chun Ni
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Author:
+    Chien-Chun Ni
+    http://www3.cs.stonybrook.edu/~chni/
+
+Reference:
+[Ni2]: Ni, C.-C., Lin, Y.-Y., Gao, J., and Gu, X. . "Network Alignment by Discrete Ollivier-Ricci Flow", Graph Drawing 2018
+
+"""
+
 from .OllivierRicci import *
 
 
