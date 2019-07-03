13f34d29506ea6ebfcde90bc58b1dc664b3e0b04
saibalmars <saibalmars@gmail.com>
2019-07-03 15:43:52 -0700

    fix version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="GraphRicciCurvature",
-    version="0.1.1",
+    version="0.1",
     author="Chien-Chun Ni",
     author_email="saibalmars@gmail.com",
     description="Compute discrete Ricci curvatures and Ricci flow on NetworkX graphs.",
