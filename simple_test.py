5d4c34d6e5ae2b6d82bfc8d2e2abd675aaa84b9a
saiba <saibalmars@gmail.com>
2020-02-07 13:03:59 -0800

    more test

--- simple_test.py
+++ simple_test.py
@@ -1,6 +1,5 @@
-import cProfile
-
 import networkx as nx
+from memory_profiler import profile
 from psutil import cpu_count
 
 from GraphRicciCurvature.OllivierRicci import OllivierRicci
@@ -13,12 +12,14 @@ proc = cpu_count(logical=False)
 # orc = OllivierRicci(G, alpha=0.5, method="Sinkhorn", verbose="DEBUG")
 # orc.compute_ricci_curvature()
 
-G_rr = nx.random_regular_graph(8, 1000)
+G_rr = nx.random_regular_graph(8, 3000)
 
 
+@profile
 def test():
     orc_rr = OllivierRicci(G_rr, proc=proc, verbose="INFO")
     orc_rr.compute_ricci_curvature()
 
 
-cProfile.run("test()")
+test()
+# cProfile.run("test()")
