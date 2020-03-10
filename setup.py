2b51bdb2bf9a393f5e11ef08c9887132e9e2bd62
saibalmars <saibalmars@gmail.com>
2020-03-09 17:39:46 -0700

    Add requirements.txt

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import setuptools
+from pip.req import parse_requirements
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -12,14 +13,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/saibalmars/GraphRicciCurvature",
-    install_requires=[
-        "networkx",
-        "numpy",
-        "cython",
-        "cvxpy",
-        "pot",
-        "networkit>=6.0"
-    ],
+    install_requires=parse_requirements("requirements.txt"),
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
